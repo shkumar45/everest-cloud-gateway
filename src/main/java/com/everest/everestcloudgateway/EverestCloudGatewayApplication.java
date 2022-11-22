a747546a0cec1bdeb43ebbf5d18bd41fa3853d69
Shankar Kumar <kumar.shankar@gmail.com>
2022-11-22 12:20:49 -0800

    first commit

---
+++ src/main/java/com/everest/everestcloudgateway/EverestCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.everest.everestcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverestCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverestCloudGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everest/everestcloudgateway/EverestCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everest.everestcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverestCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
